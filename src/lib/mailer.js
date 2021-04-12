d1ae22c3278ca5b3dd125205824a87302e13c46c
dimasdevspro <dimasdevspro@github.com>
2021-04-12 15:07:52 -0300

    initiate formatting and identification V

--- src/lib/mailer.js
+++ src/lib/mailer.js
@@ -1,10 +1,10 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require("nodemailer");
 
 module.exports = nodemailer.createTransport({
   host: "smtp.mailtrap.io",
   port: 2525,
   auth: {
     user: "24bc45eb848849",
-    pass: "8d90923d6cfe30"
-  }
+    pass: "8d90923d6cfe30",
+  },
 });
